0e2aef8fc5437e663b73cf433db0eeac2f048d29
bakNa2t <ok2.lock@gmail.com>
2024-11-21 13:27:11 +0300

    Updated GlobalStyles theme colors

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ const AppContainer = styled.div`
   width: 100vw;
   height: 100vh;
   overflow: hidden;
-  background-color: #8b5cf6;
 `;
 
 function App() {
--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,6 +1,30 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
+:root {
+    &, &.light-mode {
+        --bgr: #f1f5f9;
+        --text: #020617;
+        --color-brand: #ef4444;
+        --color-grey-1: #9ca3af;
+        --color-grey-2: #6b7280;
+        --color-grey-3: #4b5563;
+        --color-blue: #3b82f6;
+        --color-auth: #525252;
+        --shadow-temp: #0E0E0E;
+        --color-white: #fafafa;
+        --color-black: #0c0a09;
+        --color-divider: #475569;
+        --color-backdrop: #0a0a0a;
+    }
+
+    &.dark-mode {
+        --bgr: #0f172a;
+        --text: #e2e8f0;
+    }
+}
+
+
     *,
 *::before,
 *::after {
@@ -17,6 +41,18 @@ const GlobalStyles = createGlobalStyle`
   -webkit-box-sizing: border-box;
   -moz-box-sizing: border-box;
   -ms-box-sizing: border-box;
+}
+
+::-webkit-scrollbar {
+    width: 0;
+    background: transparent;
+}
+
+-ms-overflow-style: none;
+scrollbar-width: none;
+
+&::-webkit-scrollbar {
+    display: none;
 }
 `;
 
