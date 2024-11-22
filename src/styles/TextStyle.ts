34b91f18d625d6456e73e82e6b3384e2da7580e5
bakNa2t <ok2.lock@gmail.com>
2024-11-22 12:16:16 +0300

    Added TextStyle comp

---
+++ src/styles/TextStyle.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Text = styled.p`
+  color: ${({ theme }) => theme.text};
+  font-size: 14px;
+
+  &.logo {
+    letter-spacing: -1px;
+    font-size: 20px;
+    font-weight: 600;
+    font-family: "M PLUS Code Latin", sans-serif;
+  }
+`;
