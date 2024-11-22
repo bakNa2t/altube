9c292933ae17bef422b266766a54fcf4298b5149
bakNa2t <ok2.lock@gmail.com>
2024-11-22 10:48:52 +0300

    Added Rightbox comp

---
+++ src/components/header/Rightbox.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const StyledRightbox = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  gap: 0.8rem;
+`;
+
+const Rightbox = () => {
+  return <StyledRightbox>Rightbox</StyledRightbox>;
+};
+
+export default Rightbox;
