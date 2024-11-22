88b3a03e9fb59b51749dff763158581d763eacf8
bakNa2t <ok2.lock@gmail.com>
2024-11-22 10:47:24 +0300

    Added Leftbox comp

---
+++ src/components/header/Leftbox.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const StyledLeftbox = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
+
+const Leftbox = () => {
+  return <StyledLeftbox>Leftbox</StyledLeftbox>;
+};
+
+export default Leftbox;
