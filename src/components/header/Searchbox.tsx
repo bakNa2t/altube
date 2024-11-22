bda9304a98dd7a0778e63fffb1c11d28c6862f1f
bakNa2t <ok2.lock@gmail.com>
2024-11-22 10:48:31 +0300

    Added Searchbox comp

---
+++ src/components/header/Searchbox.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const StyledSearchbox = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
+
+const Searchbox = () => {
+  return <StyledSearchbox>Searchbox Input</StyledSearchbox>;
+};
+
+export default Searchbox;
