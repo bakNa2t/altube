ee921ce3c459ca2d47011f85a09f5200a796a2d1
bakNa2t <ok2.lock@gmail.com>
2024-11-24 17:37:32 +0300

    Added settings_data const and SettingRow

--- src/components/settings/Settings.tsx
+++ src/components/settings/Settings.tsx
@@ -1,4 +1,8 @@
 import styled from "styled-components";
+import { HiLanguage } from "react-icons/hi2";
+import { GoMoon } from "react-icons/go";
+
+import SettingRow from "./SettingRow";
 
 const StyledSettings = styled.div`
   position: absolute;
@@ -9,13 +13,34 @@ const StyledSettings = styled.div`
   width: 17.3vw;
   padding: 0.5rem 0;
   border-radius: 0.5rem;
-  background-color: ${({ theme: color_auth }) => color_auth};
-  box-shadow: 0px 10px 43px -3px ${({ theme: shadow_temp }) => shadow_temp};
+  background-color: ${({ theme: { color_auth } }) => color_auth};
+  box-shadow: 0px 10px 23px -3px ${({ theme: { shadow_temp } }) => shadow_temp};
   z-index: 1000;
 `;
 
 const Settings = () => {
-  return <StyledSettings>Settings</StyledSettings>;
+  const SETTINGS_DATA = [
+    {
+      label: "Language",
+      icon: <HiLanguage size={20} />,
+      value: "English",
+      onClick: () => null,
+    },
+    {
+      label: "Apperance",
+      icon: <GoMoon size={20} />,
+      value: "Theme",
+      onClick: () => null,
+    },
+  ];
+
+  return (
+    <StyledSettings>
+      {SETTINGS_DATA.map(({ label, icon, value }) => (
+        <SettingRow key={label} label={label} icon={icon} value={value} />
+      ))}
+    </StyledSettings>
+  );
 };
 
 export default Settings;
