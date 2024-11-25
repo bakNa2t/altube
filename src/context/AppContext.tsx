71822c6f0562ad495eff7f40f2bb9ab0b4c86a23
bakNa2t <ok2.lock@gmail.com>
2024-11-25 11:28:10 +0300

    Added language to context

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,7 +1,12 @@
 import { createContext, ReactNode, useContext, useState } from "react";
 
+import { ITranslations, LANGUAGE } from "../utils/translations";
+
 interface IAppContextVlaue {
   theme: "light" | "dark";
+  language: "english" | "russian";
+  toggleLanguage: () => void;
+  text: ITranslations;
 }
 
 interface IAppContextProps {
@@ -12,9 +17,17 @@ const AppContext = createContext<IAppContextVlaue | null>(null);
 
 export const AppContextProvider = ({ children }: IAppContextProps) => {
   const [theme /*, setTheme*/] = useState<"light" | "dark">("dark");
+  const [language, setLanguage] = useState<"english" | "russian">("english");
+
+  const toggleLanguage = () => {
+    setLanguage((language) => (language === "english" ? "russian" : "english"));
+  };
 
   const value = {
     theme,
+    language,
+    toggleLanguage,
+    text: LANGUAGE[language],
   };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
