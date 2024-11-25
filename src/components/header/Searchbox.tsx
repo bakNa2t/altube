db7d56cfd97d71fa1836d9b8092b5a40874c41d9
bakNa2t <ok2.lock@gmail.com>
2024-11-25 14:38:38 +0300

    Added SearchBar styled comp

--- src/components/header/Searchbox.tsx
+++ src/components/header/Searchbox.tsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 import styled from "styled-components";
+
+import { useAppContext } from "../../context/AppContext";
 
 const StyledSearchbox = styled.div`
   display: flex;
@@ -6,8 +9,38 @@ const StyledSearchbox = styled.div`
   gap: 1rem;
 `;
 
+const StyledSearchBar = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 3.5rem;
+  width: 33.5vw;
+  border-radius: 100rem;
+  border: 1px solid ${({ theme: { color_grey_2 } }) => color_grey_2};
+  background-color: ${({ theme: { color_grey_2 } }) => color_grey_2};
+  overflow: hidden;
+
+  input {
+    font-size: inherit;
+    padding-left: 1rem;
+    color: ${({ theme: { text } }) => text};
+    background-color: ${({ theme: { bgr } }) => bgr};
+  }
+`;
+
 const Searchbox = () => {
-  return <StyledSearchbox>Searchbox Input</StyledSearchbox>;
+  const [searchValue, setSearchValue] = useState("");
+  const { text } = useAppContext();
+
+  return (
+    <StyledSearchbox>
+      <StyledSearchBar>
+        <input
+          value={searchValue}
+          placeholder={text.search}
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
+      </StyledSearchBar>
+    </StyledSearchbox>
+  );
 };
 
 export default Searchbox;
