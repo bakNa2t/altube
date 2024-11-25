0f6defcfa890febad26226983efa1946c046579d
bakNa2t <ok2.lock@gmail.com>
2024-11-25 10:58:06 +0300

    Created translations for toggle lang

---
+++ src/utils/translations.ts
@@ -0,0 +1,185 @@
+export interface ITranslations {
+  signIn: string;
+  settings: string;
+  appreance: string;
+  dark: string;
+  light: string;
+  language: string;
+  english: string;
+  russian: string;
+  search: string;
+  voiceSearch: string;
+  history: string;
+  you: string;
+  subscriptions: string;
+  shorts: string;
+  home: string;
+  reportHistory: string;
+  help: string;
+  sendFeedback: string;
+  moreFromYoutube: string;
+  youtubePremium: string;
+  youtubeMusic: string;
+  youtubeKids: string;
+  browseChannels: string;
+  explore: string;
+  trending: string;
+  music: string;
+  moviesTv: string;
+  live: string;
+  gaming: string;
+  news: string;
+  sport: string;
+  learning: string;
+  fasionBeauty: string;
+  podcasts: string;
+  signInMenuText: string;
+  all: string;
+  sports: string;
+  boxing: string;
+  gym: string;
+  flowers: string;
+  soccer: string;
+  tennis: string;
+  tesla: string;
+  basketball: string;
+  housing: string;
+  finance: string;
+  stocks: string;
+  technology: string;
+  apple: string;
+  fasion: string;
+  showMore: string;
+  showLess: string;
+  save: string;
+  share: string;
+  subscribe: string;
+  views: string;
+  subscribers: string;
+}
+
+const english: ITranslations = {
+  signIn: "Sign In",
+  settings: "Settings",
+  appreance: "Appreance",
+  dark: "Dark",
+  light: "Light",
+  language: "Language",
+  english: "English",
+  russian: "Russian",
+  search: "Search",
+  voiceSearch: "Voice Search",
+  history: "History",
+  you: "You",
+  subscriptions: "Subscriptions",
+  shorts: "Shorts",
+  home: "Home",
+  reportHistory: "Report History",
+  help: "Help",
+  sendFeedback: "Send Feedback",
+  moreFromYoutube: "More from YouTube",
+  youtubePremium: "YouTube Premium",
+  youtubeMusic: "YouTube Music",
+  youtubeKids: "YouTube Kids",
+  browseChannels: "Browse Channels",
+  explore: "Explore",
+  trending: "Trending",
+  music: "Music",
+  moviesTv: "Movies & TV",
+  live: "Live",
+  gaming: "Gaming",
+  news: "News",
+  sport: "Sport",
+  learning: "Learning",
+  fasionBeauty: "Fasion & Beauty",
+  podcasts: "Podcasts",
+  signInMenuText: "Sign in to like videos, comment, and subscribe.",
+  all: "All",
+  sports: "Sports",
+  boxing: "Boxing",
+  gym: "Gym",
+  flowers: "Flowers",
+  soccer: "Soccer",
+  tennis: "Tennis",
+  tesla: "Tesla Inc",
+  basketball: "Basketball",
+  housing: "Housing",
+  finance: "Finance",
+  stocks: "Stocks",
+  technology: "Technology",
+  apple: "Apple Inc",
+  fasion: "Fasion",
+  showMore: "Show more",
+  showLess: "Show less",
+  save: "Save",
+  share: "Share",
+  subscribe: "Subscribe",
+  views: "Views",
+  subscribers: "Subscribers",
+};
+
+const russian: ITranslations = {
+  signIn: "Войти",
+  settings: "Настройки",
+  appreance: "Внешний вид",
+  dark: "Темная",
+  light: "Светлая",
+  language: "Язык",
+  english: "Английский",
+  russian: "Русский",
+  search: "Поиск",
+  voiceSearch: "Голосовой поиск",
+  history: "История",
+  you: "Вы",
+  subscriptions: "Подписки",
+  shorts: "Shorts",
+  home: "Главная",
+  reportHistory: "История отчетов",
+  help: "Помощь",
+  sendFeedback: "Отправить отзыв",
+  moreFromYoutube: "Ещё от YouTube",
+  youtubePremium: "YouTube Премиум",
+  youtubeMusic: "YouTube Музыка",
+  youtubeKids: "YouTube Дети",
+  browseChannels: "Просмотр каналов",
+  explore: "Исследовать",
+  trending: "Тренды",
+  music: "Музыка",
+  moviesTv: "Фильмы и Сериалы",
+  live: "Прямой эфир",
+  gaming: "Игры",
+  news: "Новости",
+  sport: "Спорт",
+  learning: "Обучение",
+  fasionBeauty: "Фэшн и Красота",
+  podcasts: "Подкасты",
+  signInMenuText:
+    "Войти, чтобы поставить лайк, комментировать и подписываться.",
+  all: "Все",
+  sports: "Спорт",
+  boxing: "Бокс",
+  gym: "Тренажерная зона",
+  flowers: "Цветы",
+  soccer: "Футбол",
+  tennis: "Теннис",
+  tesla: "Tesla Inc",
+  basketball: "Баскетбол",
+  housing: "Жилье",
+  finance: "Финансы",
+  stocks: "Акции",
+  technology: "Технологии",
+  apple: "Apple Inc",
+  fasion: "Фэшн",
+  showMore: "Показать больше",
+  showLess: "Показать меньше",
+  save: "Сохранить",
+  share: "Поделиться",
+  subscribe: "Подписаться",
+  views: "Просмотры",
+  subscribers: "Подписчики",
+};
+
+export const LANGUAGE = {
+  english,
+  russian,
+};
