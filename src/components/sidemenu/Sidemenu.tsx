9dfd5642b61cad8ec5deb211944f124d49ef34e0
bakNa2t <ok2.lock@gmail.com>
2024-11-27 15:24:12 +0300

    Added full sidemenu nav list

--- src/components/sidemenu/Sidemenu.tsx
+++ src/components/sidemenu/Sidemenu.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { Text } from "../../styles/TextStyle";
 
 import { useAppContext } from "../../context/AppContext";
-import { MENU_ITEMS_SHORT } from "../../utils/MenuItems";
+import { MENU_ITEMS_FULL, MENU_ITEMS_SHORT } from "../../utils/MenuItems";
 import { ITranslations } from "../../utils/translations";
 
 const StyledSidemenu = styled.div`
@@ -11,12 +11,30 @@ const StyledSidemenu = styled.div`
   overflow-y: scroll;
   overflow-x: hidden;
   padding: 0rem 0.5rem;
+
+  .title {
+    font-size: 0.85rem;
+    margin: 0 0 0.5rem 1.7rem;
+    font-weight: bold;
+  }
 `;
 
-const StyledSideMenuRow = styled.div`
+const StyledSideMenuFullRow = styled.div`
+  padding: 0.5rem 0;
+  border-bottom: 1px solid ${({ theme: { color_divider } }) => color_divider};
+`;
+
+const StyledSideMenuShortRow = styled.div`
   display: flex;
   border-radius: 0.5rem;
   color: ${({ theme: { text } }) => text};
+
+  .text {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding: 1.1rem 0 1.1rem 1.5rem;
+  }
 
   &.short {
     font-size: 1.4rem;
@@ -29,6 +47,14 @@ const StyledSideMenuRow = styled.div`
       font-size: 8px;
     }
   }
+  &.full {
+    height: 2.5rem;
+    font-size: 1.5rem;
+    align-items: center;
+    margin-left: 0.75rem;
+    padding-left: 1rem;
+    gap: 1.3rem;
+  }
 
   &:hover {
     background-color: ${({ theme: { color_grey_2 } }) => color_grey_2};
@@ -39,20 +65,32 @@ const StyledSideMenuRow = styled.div`
 const Sidemenu = () => {
   const { isSideMenuShort, text } = useAppContext();
 
-  if (isSideMenuShort) {
-    return (
-      <StyledSidemenu>
-        {MENU_ITEMS_SHORT.map(({ name, icon }) => (
-          <StyledSideMenuRow className="short" key={name}>
-            {icon}
-            <Text>{text[name as keyof ITranslations]}</Text>
-          </StyledSideMenuRow>
-        ))}
-      </StyledSidemenu>
-    );
-  }
-
-  return <StyledSidemenu>Sidemenu</StyledSidemenu>;
+  return isSideMenuShort ? (
+    <StyledSidemenu>
+      {MENU_ITEMS_SHORT.map(({ name, icon }) => (
+        <StyledSideMenuShortRow className="short" key={name}>
+          {icon}
+          <Text>{text[name as keyof ITranslations]}</Text>
+        </StyledSideMenuShortRow>
+      ))}
+    </StyledSidemenu>
+  ) : (
+    <StyledSidemenu>
+      {MENU_ITEMS_FULL.map(({ title, list }, index) => (
+        <StyledSideMenuFullRow key={index}>
+          {title && (
+            <Text className="title">{text[title as keyof ITranslations]}</Text>
+          )}
+          {list.map(({ name, icon }) => (
+            <StyledSideMenuShortRow className="full" key={name}>
+              {icon}
+              <Text>{text[name as keyof ITranslations]}</Text>
+            </StyledSideMenuShortRow>
+          ))}
+        </StyledSideMenuFullRow>
+      ))}
+    </StyledSidemenu>
+  );
 };
 
 export default Sidemenu;
