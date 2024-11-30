e00fcd52435d98b8c3d29cb3bc00880e5525f5bf
bakNa2t <ok2.lock@gmail.com>
2024-11-30 10:31:36 +0300

    Added Backdrop while fetching

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -2,6 +2,8 @@ import styled from "styled-components";
 
 import Categories from "./Categories";
 
+import { useAppContext } from "../../context/AppContext";
+
 const StyledContent = styled.section`
   position: relative;
   display: grid;
@@ -11,10 +13,22 @@ const StyledContent = styled.section`
   height: 94.2%;
 `;
 
+const Backdrop = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 94.2%;
+  background-color: ${({ theme: { bgr } }) => bgr};
+  z-index: 1000;
+  opacity: 0.8;
+`;
+
 const Content = () => {
+  const { isFetchingVideos } = useAppContext();
+
   return (
     <StyledContent>
       <Categories />
+      {isFetchingVideos && <Backdrop />}
     </StyledContent>
   );
 };
