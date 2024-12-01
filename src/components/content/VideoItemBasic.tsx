ed2d470fce53fe6dba11f54f7a1e83e96c8bf9f3
bakNa2t <ok2.lock@gmail.com>
2024-12-01 12:46:34 +0300

    Created VideoItemBasic

---
+++ src/components/content/VideoItemBasic.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const StyledVideoItemBasic = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  gap: 0.8rem;
+`;
+
+const VideoItemBasic = () => {
+  return <StyledVideoItemBasic>VideoItemBasic</StyledVideoItemBasic>;
+};
+
+export default VideoItemBasic;
