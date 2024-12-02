608430ef2119e91719d864ee53fc7e7f7819d1c4
bakNa2t <ok2.lock@gmail.com>
2024-12-02 11:38:40 +0300

    Added request's options

--- src/utils/rapid-api/config.ts
+++ src/utils/rapid-api/config.ts
@@ -1,1 +1,14 @@
-export const rapidApiKey = import.meta.env.VITE_RAPID_API_KEY;
+export const rapidConfig = {
+  api_key: import.meta.env.VITE_RAPID_API_KEY,
+};
+
+export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+export const HOST = "youtube-v31.p.rapidapi.com";
+
+export const options = {
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": rapidConfig.api_key,
+    "x-rapidapi-host": HOST,
+  },
+};
