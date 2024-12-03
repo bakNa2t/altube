0bec904d8ac4de601fcee210074971b084294319
bakNa2t <ok2.lock@gmail.com>
2024-12-03 13:47:47 +0300

    Added react-player to item

--- src/components/content/VideoItemBasic.tsx
+++ src/components/content/VideoItemBasic.tsx
@@ -1,14 +1,50 @@
+import { useState } from "react";
+import ReactPlayer from "react-player";
 import styled from "styled-components";
+
+const url = "https://www.youtube.com/watch?v=";
 
 const StyledVideoItemBasic = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
+  height: 10rem;
   gap: 0.8rem;
+  background-color: ${({ theme: { color_black } }) => color_black};
+
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
-const VideoItemBasic = () => {
-  return <StyledVideoItemBasic>VideoItemBasic</StyledVideoItemBasic>;
+interface IVideoItemBasicProps {
+  dataVideos: string;
+}
+
+const VideoItemBasic = ({ dataVideos }: IVideoItemBasicProps) => {
+  const [playPreviewVideo, setPlayPreviewVideo] = useState(false);
+
+  return (
+    <StyledVideoItemBasic
+      onMouseOver={() => setPlayPreviewVideo(true)}
+      onMouseOut={() => setPlayPreviewVideo(false)}
+    >
+      {playPreviewVideo ? (
+        <ReactPlayer
+          width="100%"
+          height="100%"
+          controls={false}
+          volume={1}
+          muted={false}
+          playing={playPreviewVideo}
+          url={`${url}${dataVideos.id.videoId}`}
+          style={{ width: "100%", height: "100%" }}
+        />
+      ) : (
+        <img src={dataVideos.snippet.thumbnails.medium.url} alt="thmabnail" />
+      )}
+    </StyledVideoItemBasic>
+  );
 };
 
 export default VideoItemBasic;
