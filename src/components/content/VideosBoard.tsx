35c258937222134cdaa353360b9104dee5221d3f
bakNa2t <ok2.lock@gmail.com>
2024-12-03 13:55:20 +0300

    Added VideosThumbnailsContainer and VideoItemBasic to VideosBoard

--- src/components/content/VideosBoard.tsx
+++ src/components/content/VideosBoard.tsx
@@ -1,13 +1,31 @@
 import styled from "styled-components";
 
+import VideosThumbnailsContainer from "./VideosThumbnailsContainer";
+import VideoItemBasic from "./VideoItemBasic";
+
+import { useAppContext } from "../../context/AppContext";
+
 const StyledVideosBoard = styled.div`
   height: 88.7vh;
   padding: 1.6rem 1.5rem 0 0;
   overflow-y: scroll;
-  background-color: ${({ theme: { color_grey_1 } }) => color_grey_1};
 `;
 const VideosBoard = () => {
-  return <StyledVideosBoard>VideosBoard</StyledVideosBoard>;
+  const { dataVideos } = useAppContext();
+
+  const UP_VIDEO_BLOCK = dataVideos.slice(0, 8);
+  // const MIDDLE_VIDEO_BLOCK = dataVideos.slice(8, 20);
+  // const DOWN_VIDEO_BLOCK = dataVideos.slice(20, 28);
+
+  return (
+    <StyledVideosBoard>
+      <VideosThumbnailsContainer>
+        {UP_VIDEO_BLOCK.map((video, index) => (
+          <VideoItemBasic dataVideos={video} key={index}></VideoItemBasic>
+        ))}
+      </VideosThumbnailsContainer>
+    </StyledVideosBoard>
+  );
 };
 
 export default VideosBoard;
