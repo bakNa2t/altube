9ed4923eabbf3a89a26e746bfc1d9b86ea346da1
bakNa2t <ok2.lock@gmail.com>
2024-12-05 15:16:11 +0300

    Added VideosThumbnailContainer with DOWN_VIDEO_BLOCK

--- src/components/content/VideosBoard.tsx
+++ src/components/content/VideosBoard.tsx
@@ -8,7 +8,7 @@ import { useAppContext } from "../../context/AppContext";
 
 const StyledVideosBoard = styled.div`
   height: 88.7vh;
-  padding: 1.6rem 1.5rem 0 0;
+  padding: 1.6rem 1.5rem 2rem 0;
   overflow-y: scroll;
 `;
 const VideosBoard = () => {
@@ -16,7 +16,7 @@ const VideosBoard = () => {
 
   const UP_VIDEO_BLOCK = dataVideos.slice(0, 8);
   const SHORTS_VIDEO_BLOCK = dataVideos.slice(8, 20);
-  // const DOWN_VIDEO_BLOCK = dataVideos.slice(20, 28);
+  const DOWN_VIDEO_BLOCK = dataVideos.slice(20, 28);
 
   return (
     <StyledVideosBoard>
@@ -26,6 +26,11 @@ const VideosBoard = () => {
         ))}
       </VideosThumbnailsContainer>
       <VideoShorts dataVideos={SHORTS_VIDEO_BLOCK} />
+      <VideosThumbnailsContainer>
+        {DOWN_VIDEO_BLOCK.map((video, index) => (
+          <VideoItemBasic dataVideos={video} key={index}></VideoItemBasic>
+        ))}
+      </VideosThumbnailsContainer>
     </StyledVideosBoard>
   );
 };
