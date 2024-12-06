0625fa1bf4afd423d086d5d96f580ac625d8646b
bakNa2t <ok2.lock@gmail.com>
2024-12-06 14:09:02 +0300

    Added VideosSuggestionContainer

--- src/components/content/VideoItemWatch.tsx
+++ src/components/content/VideoItemWatch.tsx
@@ -1,9 +1,47 @@
 import styled from "styled-components";
 
-const StyledVideoItemWatch = styled.div``;
+import VideoItemBasic from "./VideoItemBasic";
+
+import { useAppContext } from "../../context/AppContext";
+
+const StyledVideoItemWatch = styled.div`
+  display: grid;
+  grid-template-columns: 71.5vw 24vw;
+  width: 100%;
+  height: 94.2vh;
+  gap: 1.5rem;
+  padding: 0 1.5vw;
+  overflow: scroll;
+`;
+
+const VideosSuggestionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  width: 100%;
+  padding-top: 1.5rem;
+
+  h3 {
+    font-size: 1.4rem;
+    font-weight: bold;
+    color: ${({ theme: { text } }) => text};
+  }
+`;
 
 const VideoItemWatch = () => {
-  return <StyledVideoItemWatch>VideoItemWatch</StyledVideoItemWatch>;
+  const { dataVideos } = useAppContext();
+
+  return (
+    <StyledVideoItemWatch>
+      <div>Video Player</div>
+      <VideosSuggestionContainer>
+        <h3>Suggestion's Videos</h3>
+        {dataVideos.map((video, index) => (
+          <VideoItemBasic dataVideos={video} key={index} />
+        ))}
+      </VideosSuggestionContainer>
+    </StyledVideoItemWatch>
+  );
 };
 
 export default VideoItemWatch;
