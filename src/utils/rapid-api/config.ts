9baed8595e744af884f65d0718dd7be704e172d8
bakNa2t <ok2.lock@gmail.com>
2024-12-06 19:09:21 +0300

    Added url to fetch certain video

--- src/utils/rapid-api/config.ts
+++ src/utils/rapid-api/config.ts
@@ -3,6 +3,8 @@ export const rapidConfig = {
 };
 
 export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+export const VIDEO_URL =
+  "https://youtube-v31.p.rapidapi.com/videos?part=contentDetails%2Csnippet%2Cstatistics&id=";
 export const HOST = "youtube-v31.p.rapidapi.com";
 
 export const options = {
