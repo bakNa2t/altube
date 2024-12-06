8ee0bf99db26f0e678bbd3b92648ac46ddb7cacb
bakNa2t <ok2.lock@gmail.com>
2024-12-06 12:35:35 +0300

    Created VideoItemWatch

---
+++ src/components/content/VideoItemWatch.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StyledVideoItemWatch = styled.div``;
+
+const VideoItemWatch = () => {
+  return <StyledVideoItemWatch>VideoItemWatch</StyledVideoItemWatch>;
+};
+
+export default VideoItemWatch;
