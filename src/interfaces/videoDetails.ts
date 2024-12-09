be82865780e4614761d0493563545a0be2d01e16
bakNa2t <ok2.lock@gmail.com>
2024-12-09 10:53:44 +0300

    Created videoDetails interface

---
+++ src/interfaces/videoDetails.ts
@@ -0,0 +1,44 @@
+import { Thumbnails } from "./videos";
+
+export interface IVideoDetailsProps {
+  contentDetails: IContentDetails;
+  id: string;
+  kind?: string;
+  snippet: Snippet;
+  statistics: Statistics;
+}
+
+export interface IContentDetails {
+  caption: boolean;
+  contentRating?: object;
+  definition: string;
+  dimension: string;
+  duration: string;
+  licensedContent: boolean;
+  projection: string;
+}
+
+export interface Snippet {
+  categoryId: string;
+  channelId: string;
+  channelTitle: string;
+  defaultAudioLanguage: string;
+  description: string;
+  liveBroadcastContent: string;
+  localized: Localized;
+  publishedAt: string;
+  thumbnails: Thumbnails;
+  title: string;
+}
+
+export interface Localized {
+  description: string;
+  title: string;
+}
+
+export interface Statistics {
+  commentCount: string;
+  favoriteCount: string;
+  likeCount: string;
+  viewCount: string;
+}
