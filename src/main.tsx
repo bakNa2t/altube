9bf679efbf75931d7d0977c42a6f0895f28407e6
bakNa2t <ok2.lock@gmail.com>
2024-12-10 15:35:04 +0300

    Added Provider to use store

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import { AppContextProvider } from "./context/AppContext.tsx";
 import App from "./App.tsx";
+import { store } from "./store/store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <AppContextProvider>
-        <App />
-      </AppContextProvider>
+      <Provider store={store}>
+        <AppContextProvider>
+          <App />
+        </AppContextProvider>
+      </Provider>
     </BrowserRouter>
   </StrictMode>
 );
