f6dc704f0337fbf38236af726eba9808c3e49602
bakNa2t <ok2.lock@gmail.com>
2024-12-11 15:00:03 +0300

    Added switchTranslation reducer

--- src/store/appSlice.ts
+++ src/store/appSlice.ts
@@ -2,20 +2,26 @@ import { createSlice } from "@reduxjs/toolkit";
 
 interface IAppState {
   theme: "light" | "dark";
+  language: "english" | "russian";
 }
 
 const initialState: IAppState = {
   theme: "dark",
+  language: "english",
 };
 
 export const appSlice = createSlice({
   name: "app",
   initialState,
   reducers: {
-    swapTheme(state) {
+    switchThemeColor(state) {
       state.theme = state.theme === "light" ? "dark" : "light";
+    },
+
+    switchTranslation(state) {
+      state.language = state.language === "english" ? "russian" : "english";
     },
   },
 });
 
-export const { swapTheme } = appSlice.actions;
+export const { switchThemeColor, switchTranslation } = appSlice.actions;
