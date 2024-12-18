20ea4e093a07ec9f284c5ce7786aff77f4cebc98
bakNa2t <ok2.lock@gmail.com>
2024-12-18 16:12:59 +0300

    Added channels_videos_url const

--- src/utils/rapid-api/config.ts
+++ src/utils/rapid-api/config.ts
@@ -10,6 +10,7 @@ export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
 export const VIDEO_URL = `${BASE_URL}/videos?part=contentDetails%2Csnippet%2Cstatistics&id=`;
 export const CHANNEL_URL = `${BASE_URL}/channels?part=snippet%2Cstatistics&id=`;
 export const COMMENTS_URL = `${BASE_URL}/commentThreads?part=snippet&videoId=`;
+export const CHANNELS_VIDEOS_URL = `${BASE_URL}/search?channelId=`;
 export const HOST = "youtube-v31.p.rapidapi.com";
 
 export const options = {
