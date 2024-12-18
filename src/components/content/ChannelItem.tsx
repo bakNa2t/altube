c79e01d37e18b9c20e8a9b50c4234f90087dcafc
bakNa2t <ok2.lock@gmail.com>
2024-12-18 12:01:54 +0300

    Added ChannelBanner styled comp

--- src/components/content/ChannelItem.tsx
+++ src/components/content/ChannelItem.tsx
@@ -1,13 +1,42 @@
+import styled from "styled-components";
+
 import { useAppContext } from "../../context/AppContext";
 import { Text } from "../../styles/TextStyle";
+
+const StyledChannelItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0 4rem;
+`;
+
+const ChannelBanner = styled.div`
+  width: 100%;
+  height: 15rem;
+  overflow: hidden;
+  border-radius: 1rem;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
 
 const ChannelItem = () => {
   const { fetchChannelDetails } = useAppContext();
 
+  console.log(fetchChannelDetails);
+
   return (
-    <div>
+    <StyledChannelItem>
+      <ChannelBanner>
+        <img
+          src={fetchChannelDetails?.brandingSettings?.image?.bannerExternalUrl}
+          alt="banner"
+        />
+      </ChannelBanner>
       <Text>{fetchChannelDetails?.snippet?.title}</Text>
-    </div>
+    </StyledChannelItem>
   );
 };
 
