ac6d8fe578a334af398bf542f0d09478f87f112a
bakNa2t <ok2.lock@gmail.com>
2024-12-24 13:01:22 +0300

    Added convertToInternationalCurrencySystem fn

--- src/utils/func.ts
+++ src/utils/func.ts
@@ -54,3 +54,18 @@ export const formatCountSubscriber = (value: string): string => {
     return number.toString();
   }
 };
+
+export const convertToInternationalCurrencySystem = (
+  value: string
+): string | number => {
+  // Nine Zeroes for Billions
+  return Math.abs(Number(value)) >= 1.0e9
+    ? (Math.abs(Number(value)) / 1.0e9).toFixed(2) + "B"
+    : // Six Zeroes for Millions
+    Math.abs(Number(value)) >= 1.0e6
+    ? (Math.abs(Number(value)) / 1.0e6).toFixed(2) + "M"
+    : // Three Zeroes for Thousands
+    Math.abs(Number(value)) >= 1.0e3
+    ? (Math.abs(Number(value)) / 1.0e3).toFixed(2) + "K"
+    : Math.abs(Number(value));
+};
