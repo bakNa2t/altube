43b254b70d2fddde0c732176b2692e7c77f14d57
bakNa2t <ok2.lock@gmail.com>
2024-12-24 14:27:10 +0300

    Added playlists videos url const

--- src/utils/rapid-api/config.ts
+++ src/utils/rapid-api/config.ts
@@ -6,12 +6,14 @@ export const rapidConfig: IRapidConfigProps = {
   api_key: import.meta.env.VITE_RAPID_API_KEY,
 };
 
+export const HOST = "youtube-v31.p.rapidapi.com";
+
 export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
 export const VIDEO_URL = `${BASE_URL}/videos?part=contentDetails%2Csnippet%2Cstatistics&id=`;
 export const CHANNEL_URL = `${BASE_URL}/channels?part=snippet%2Cstatistics&id=`;
 export const COMMENTS_URL = `${BASE_URL}/commentThreads?part=snippet&videoId=`;
 export const CHANNELS_VIDEOS_URL = `${BASE_URL}/search?channelId=`;
-export const HOST = "youtube-v31.p.rapidapi.com";
+export const CHANNEL_PLAYLISTS_VIDEOS_URL = `${BASE_URL}/playlistItems?playlistId=`;
 
 export const options = {
   method: "GET",
